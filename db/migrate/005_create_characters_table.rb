4fe1573d917c5a6ca6de712c6ccb835125d5f5b2
Elijah wines <elijahwines84@yahoo.com>
2020-04-23 16:22:24 +0000

    Automatically backed up by Learn

--- db/migrate/005_create_characters_table.rb
+++ db/migrate/005_create_characters_table.rb
@@ -1,7 +1,7 @@
 class CreateCharactersTable
-  def change 
+  def change
     create_table :characters do |t|
-      t.string :name 
+      t.string :name
       t.integer :actor_id
       t.integer :show_id
     end
